15064e4069540226e219c4e749b95c4c05475a9b
cps97 <colinstoutt@gmail.com>
2022-10-12 22:04:29 -0700

    setup server

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const budget = require("./models/budget.js");
-
 const express = require("express");
 const app = express();
 const port = 3000;
 
+//middleware
+app.use(express.urlencoded({ extended: false }));
+
+// INDEX route
 app.get("/", (req, res) => {
-  res.send("Hola");
+  res.send(budget);
 });
 
 app.listen(port, () => {
